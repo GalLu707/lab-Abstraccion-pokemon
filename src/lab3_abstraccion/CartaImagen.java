ae0ea7d303a76f60ff7379a72482e598808d9abf
GalLu707 <zasseuwu@gmail.com>
2026-02-06 10:19:40 -0600

    no da error mi parte yipiee

--- src/lab3_abstraccion/CartaImagen.java
+++ src/lab3_abstraccion/CartaImagen.java
@@ -1,18 +1,50 @@
-
 package lab3_abstraccion;
 
-<<<<<<< HEAD
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Image;
 
-public class CartaImagen {
-=======
-/**
- *
- * @author USER
- */
 public class CartaImagen extends Cartas {
-    
-    
-    
->>>>>>> a513ddb390b002a9aaeb1397b09840e425401e8c
-    
+
+   
+    private JButton boton;
+    private ImageIcon imagenFrontal;
+    private ImageIcon imagenTrasera;
+
+    public CartaImagen(String rutaImagen, JButton boton) {
+        super(rutaImagen); 
+        this.boton = boton;
+
+        this.imagenFrontal = escalarImagen(new ImageIcon(rutaImagen));
+        this.imagenTrasera = escalarImagen(new ImageIcon("src/img/back.png"));
+
+        ocultar();
+    }
+
+    @Override
+    public void mostrar() {
+        this.descubierta = true;
+        this.boton.setIcon(imagenFrontal);
+        this.boton.setEnabled(false); 
+    }
+
+    @Override
+    public void ocultar() {
+        this.descubierta = false;
+        this.boton.setIcon(imagenTrasera);
+        this.boton.setEnabled(true);
+    }
+
+    private ImageIcon escalarImagen(ImageIcon icono) {
+        Image img = icono.getImage();
+       
+        Image newimg = img.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(newimg);
+    }
+
+    public JButton getBoton() {
+        return boton;
+    }
 }
+
+
