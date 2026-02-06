199f84329350e269dc9b7506b20918dc1ff77959
JeanNajar <jean_carlos@unitec.edu>
2026-02-06 10:49:01 -0600

    Clases 2.1

--- src/lab3_abstraccion/Gui.java
+++ src/lab3_abstraccion/Gui.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 
 
 public class Gui extends JFrame implements Acciones{
+    
     private Cartas[] cartas = new Cartas[36];
     private Jugadores j1, j2;
     private int turnoActu = 1;
@@ -18,14 +19,15 @@ public class Gui extends JFrame implements Acciones{
     public void cargarCartas(){
         try{
             ArrayList<String> imagenes = new ArrayList<>();
+            
             for (int i = 0; i < 18; i++) {
-                imagenes.add("src/Imagenes"+i+".png");
-                imagenes.add("src/Imagenes"+i+".png");
+                imagenes.add("/Imagenes/" + i + ".png");
+                imagenes.add("/Imagenes/" + i + ".png");
                 
             }
             Collections.shuffle(imagenes);
             
-            for(int i =1; i<=18; i++){
+            for(int i =0; i<=18; i++){
                 cartas[i]= new CartaMemoria(imagenes.get(i));
             }
         }catch(Exception e){
@@ -43,13 +45,20 @@ public class Gui extends JFrame implements Acciones{
                 j2.incrementarAcierto();
             }
         }else{
+            
+            //cc
+            
+            cartas[p1].ocultar();
+            cartas[p2].ocultar();
+            
             cambiarTurno();
         } 
         
     }
   
     public void iniciarPartida(){
-        
+        //cc2
+         turnoActu = 1;
     }
     public void cambiarTurno(){
         
@@ -57,7 +66,10 @@ public class Gui extends JFrame implements Acciones{
         
     }
     public void finalizarPartida(){
-        
+        JOptionPane.showMessageDialog(this,
+                "Juego terminado\n" +
+                j1.getNombre() + ": " + j1.getAciertos() + "\n" +
+                j2.getNombre() + ": " + j2.getAciertos());
     }
     
     
--- src/lab3_abstraccion/Tablero.java
+++ src/lab3_abstraccion/Tablero.java
@@ -1,17 +1,14 @@
 
 package lab3_abstraccion;
 
-import java.awt.*;
+
+
 import java.util.*;
-import java.util.List;
-
-
 
 public class Tablero {
 
     private Cartas[][] cartas;
-    private int filas;
-    private int columnas;
+    private int filas, columnas;
     private int parejasEncontradas;
     private int totalParejas;
 
@@ -19,62 +16,16 @@ public class Tablero {
         this.filas = filas;
         this.columnas = columnas;
         this.cartas = new Cartas[filas][columnas];
-        this.parejasEncontradas = 0;
         this.totalParejas = (filas * columnas) / 2;
-        inicializarCartas();
     }
 
-    private void inicializarCartas() {
-
-     
-        String[] imagenes = {
-            "/imagenes/img1.png",
-            "/imagenes/img2.png",
-            "/imagenes/img3.png",
-            "/imagenes/img4.png",
-            "/imagenes/img5.png",
-            "/imagenes/img6.png",
-            "/imagenes/img7.png",
-            "/imagenes/img8.png",
-            "/imagenes/img9.png",
-            "/imagenes/img10.png",
-            "/imagenes/img11.png",
-            "/imagenes/img12.png"
-        };
-
-        List<Cartas> listaCartas = new ArrayList<>();
-
-        // Crear parejas de cartas con la misma imagen
-        for (int i = 0; i < totalParejas; i++) {
-            String id = String.valueOf((char) ('A' + i));
-            String rutaImagen = imagenes[i];
-
-            listaCartas.add(new CartaImagen(id, rutaImagen));
-            listaCartas.add(new CartaImagen(id, rutaImagen));
-        }
-
-    
-        Collections.shuffle(listaCartas);
-
-   
-        int indice = 0;
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                cartas[i][j] = listaCartas.get(indice++);
-            }
-        }
+    public void setCarta(int f, int c, Cartas carta) {
+        cartas[f][c] = carta;
     }
 
-    public Cartas getCarta(int fila, int columna) {
-        if (fila < 0 || fila >= filas || columna < 0 || columna >= columnas) {
-            System.err.println("Índice fuera de rango");
-            return null;
-        }
-        return cartas[fila][columna];
+    public Cartas getCarta(int f, int c) {
+        return cartas[f][c];
     }
-    
-    
-    
 
     public void incrementarParejasEncontradas() {
         parejasEncontradas++;
@@ -82,21 +33,5 @@ public class Tablero {
 
     public boolean juegoCompletado() {
         return parejasEncontradas == totalParejas;
-    }
-
-    public int getFilas() {
-        return filas;
-    }
-
-    public int getColumnas() {
-        return columnas;
-    }
-
-    public int getParejasEncontradas() {
-        return parejasEncontradas;
-    }
-
-    public int getTotalParejas() {
-        return totalParejas;
     }
 }
