b17ca50a11c74bbfda54d24c3de9f9534af83311
JeanNajar <jean_carlos@unitec.edu>
2026-02-06 09:37:49 -0600

    Clase Carta 1.2

--- src/lab3_abstraccion/Cartas.java
+++ src/lab3_abstraccion/Cartas.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab3_abstraccion;
 
-/**
- *
- * @author USER
- */
-public class Cartas {
+public abstract class Cartas {
     
+    protected String id;
+    protected boolean descubierta;
+    protected boolean emparejada;
+    //protected JButton boton;
+    
+    
+    public Cartas(String id){
+        this.id=id;
+        this.descubierta = false;
+        this.emparejada= false;
+        
+    }
+    
+    
+    public abstract void mostrar();
+    public abstract void ocultar();
+    
+    public void marcarComoEmparejada() {
+        this.emparejada = true;
+        this.descubierta = true;
+    }
+    
+    public boolean estaDescubierta(){
+        return descubierta;
+    }
+    
+     public boolean estaEmparejada() {
+        return emparejada;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    /* public void setBoton(JButton boton) {
+        this.boton = boton;
+    }
+    
+    public JButton getBoton() {
+        return boton;
+    }*/
+    
+    public void voltear() {
+        if (!emparejada) {
+            descubierta = !descubierta;
+            if (descubierta) {
+                mostrar();
+            } else {
+                ocultar();
+            }
+        }
+    }
 }
